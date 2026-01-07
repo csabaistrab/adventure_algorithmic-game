dd66b529ffa0f0230540944849e1c70b194f8935
csabaistrab <csabaistrab2003@gmail.com>
2026-01-07 23:21:24 +0100

    Step

--- Assets/Scripts/FininshLine.cs
+++ Assets/Scripts/FininshLine.cs
@@ -3,20 +3,37 @@ using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    // FIGYELEM: Itt a "2D" a kulcsszó a végén!
     private void OnTriggerEnter2D(Collider2D other)
     {
-        // Megnézzük, hogy a játékos ment-e neki
         if (other.CompareTag("Player"))
         {
-            Debug.Log("2D Ütközés észlelve! Pálya váltása...");
+            Debug.Log("--- ÜTKÖZÉS ÉSZLELVE! ---");
             LoadNextLevel();
         }
     }
 
     void LoadNextLevel()
     {
-        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
-        SceneManager.LoadScene(currentSceneIndex + 1);
+        // Lekérjük az adatokat
+        int jelenlegi = SceneManager.GetActiveScene().buildIndex;
+        int kovetkezo = jelenlegi + 1;
+        int osszes = SceneManager.sceneCountInBuildSettings;
+
+        // KIÍRJUK A KONZOLRA (Ezt figyeld!)
+        Debug.Log("Jelenlegi pálya indexe: " + jelenlegi);
+        Debug.Log("Következõ pálya indexe lenne: " + kovetkezo);
+        Debug.Log("Összes pálya a Build Settingsben: " + osszes);
+
+        // A döntés
+        if (kovetkezo < osszes)
+        {
+            Debug.Log("DÖNTÉS: Van következõ pálya, betöltés...");
+            SceneManager.LoadScene(kovetkezo);
+        }
+        else
+        {
+            Debug.Log("DÖNTÉS: Nincs több pálya, irány a Menü (0)!");
+            SceneManager.LoadScene(0);
+        }
     }
 }
--- Assets/Scripts/GoalTrigger.cs
+++ Assets/Scripts/GoalTrigger.cs
@@ -1,5 +1,5 @@
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class GoalTrigger : MonoBehaviour
 {
     // Ez a függvény automatikusan lefut, ha "valami" (aminek van Rigidbody-ja) belép a Triggerbe
@@ -10,6 +10,22 @@ public class GoalTrigger : MonoBehaviour
         {
             Debug.Log(" GRATULÁLOK! PÁLYA TELJESÍTVE! ");
 
+            // --- PÁLYAVÁLTÓ KÓD ELEJE ---
+            int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+            int nextSceneIndex = currentSceneIndex + 1;
+
+            // Megnézzük, van-e következõ pálya a listában
+            if (nextSceneIndex < SceneManager.sceneCountInBuildSettings)
+            {
+                // Ha van, betöltjük (pl. Level 2)
+                SceneManager.LoadScene(nextSceneIndex);
+            }
+            else
+            {
+                // Ha nincs több, visszamegyünk a Fõmenübe (0)
+                SceneManager.LoadScene(0);
+            }
+            // --- PÁLYAVÁLTÓ KÓD VÉGE ---
             // Itt késõbb leállítjuk a játékot, vagy betöltjük a kövi pályát
             // Pl: FindObjectOfType<GameManager>().LevelComplete();
         }
